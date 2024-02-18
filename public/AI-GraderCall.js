b0156a1e1e3a77e85d97788df5bb5f89359a18b1
Grant0521 <grantjohnson521@gmail.com>
2024-02-17 23:17:22 -0800

    Updated grader to include # value list

--- public/AI-GraderCall.js
+++ public/AI-GraderCall.js
@@ -3,15 +3,21 @@ import {generateGrade} from '/aigrader.js';
 
 async function grader(AIArray, userArray) {
     // Specifies desired response from openAI API
-    let improvementPrompt = "I will provide a list of: a brief set of provided sentences, followed by a response to that sentence. Grade the evenly-ordered set of sentences from 1 to 10 based on social appropriateness.\n";
+    let improvementPrompt = "I will provide a list of: a brief set of provided sentences, followed by a response to that sentence. Grade the sentences from 1 to 10 based on social appropriateness with a brief explaination for each.\n";
+    let intGradesPrompt = "I will provide a list of: a brief set of provided sentences, followed by a response to that sentence. Provide only a list of grades of the evenly-ordered set of sentences from 1 to 10 based on social appropriateness"
+    let defaultString = "";
+    
     let i = 0;
     let arrayLen = userArray.length; 
     for (i; i < arrayLen; i++) {            // Combines all sentences in arrays for one API call
-        improvementPrompt = improvementPrompt + "This was a provided sentence #" + i + ": " + AIArray[i];
-        improvementPrompt = improvementPrompt + ".\nThis was my response: " + userArray[i] + ".\n\n";
+        defaultString = defaultString + "This was a provided sentence #" + i + ": " + AIArray[i];
+        defaultString = defaultString + ".\nThis was my response: " + userArray[i] + ".\n\n";
     }
-    const aiComments = await generateGrade(improvementPrompt);          // Calls API with combined string
-    return aiComments;                                                  // Returns paragraph describing grades
+    improvementPrompt = improvementPrompt + defaultString;
+    intGradesPrompt = intGradesPrompt + defaultString;
+    const aiIntGrades = await generateGrade(intGradesPrompt);          // Calls API with combined string
+    const aiComments = await generateGrade(improvementPrompt);         // Calls API with combined string
+    return [aiComments, aiIntGrades];                                  // Returns paragraph describing grades
 }
 
 let userArray = ["Im good. I love trains!", "the locomotive because it is fast"];           // TEST INPUT - DELETE
